a70fbbf5164e6b41e43ddd64af9911b3580025c2
Luke Drogan <luke@super-wide.com>
2021-06-03 22:48:34 +0100

    adding initial index.js for models

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Location = require('./Location');
+const Categories = require('./Categories');
+const Ad = require('./Ad');
+
+module.exports = { User, Location, Categories, Ad };
