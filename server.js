0f161391b0d140912b14ecbfe67f6910223948b0
Ross White <rosswhite@outlook.com>
2021-06-04 18:27:57 +0100

    linking stylesheet and adding login form

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./routes');
 const exphbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 const hbs = exphbs.create({});
 
 app.use(routes);
