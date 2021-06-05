de06a7462789532989205063076b4a4e0ba6f23a
Ross White <rosswhite@outlook.com>
2021-06-05 16:37:39 +0100

    Creating JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,12 @@ const Sequelize = require('sequelize');
 
 // Enable access to .env variables
 require('dotenv').config();
-
+let sequelize;
 // Use environment variables to connect to database
-const sequelize = new Sequelize(
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
@@ -13,6 +16,6 @@ const sequelize = new Sequelize(
     dialect: 'mysql',
     port: 3306
   }
-);
+)};
 
 module.exports = sequelize;
