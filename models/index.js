282bfb616e6b12e40c83c7e84ede5c77b478b3b0
Oliver Saxon <oliverrsaxon@gmail.com>
2021-06-05 11:52:54 +0100

    Added model relationships in index.js

--- models/index.js
+++ models/index.js
@@ -3,4 +3,38 @@ const Location = require('./Location');
 const Categories = require('./Categories');
 const Ad = require('./Ad');
 
+// user can have multiple ads
+User.hasMany(Ad, {
+    foreignKey: 'user_id'
+})
+
+// an ad belongs to one user
+Ad.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+// an ad is located in one location
+Ad.hasOne(Location, {
+    foreignKey: 'ad_id'
+})
+
+// ad has one category
+Ad.hasOne(Categories, {
+    foreignKey: 'ad_id'
+})
+
+Categories.hasMany(Ad, {
+    foreignKey: 'category_id'
+})
+
+// user has one location
+User.hasOne(Location, {
+    foreignKey: 'user_id'
+})
+
+// locations have many users
+Location.hasMany(User, {
+    foreignKey: 'user_id'
+})
+
 module.exports = { User, Location, Categories, Ad };
