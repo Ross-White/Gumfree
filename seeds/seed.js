198efa62c5b8eaafe23f19e0da1f5757dc983fe5
Ross White <rosswhite@outlook.com>
2021-06-05 10:53:02 +0100

    item model updated and seeds added

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,10 @@
 const sequelize = require('../config/connection');
-const { User, Location, Categories, Ad } = require('../models');
+const { User, Location, Categories, Item } = require('../models');
 
 const userData = require('./userSeeds.json');
 const locationData = require('./locationSeeds.json');
 const categoryData = require('./categorySeeds.json');
-const adData = require('./adSeeds.json')
+const itemData = require('./itemSeeds.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,7 +12,7 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-  await Ad.bulkCreate(adData);
+  await Item.bulkCreate(itemData);
   await Categories.bulkCreate(categoryData);
   await Location.bulkCreate(locationData);
   process.exit(0);
