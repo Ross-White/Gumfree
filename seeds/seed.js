31761ab6485c9c9842cd7c503f7dab6978964493
Ross White <rosswhite@outlook.com>
2021-06-05 10:12:41 +0100

    seed functions

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,3 +1,21 @@
 const sequelize = require('../config/connection');
 const { User, Location, Categories, Ad } = require('../models');
 
+const userData = require('./userSeeds.json');
+const locationData = require('./locationData.json');
+const categoryData = require('./categorySeeds.json');
+const adData = require('./adSeeds.json')
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await Ad.bulkCreate(adData);
+  await Categories.bulkCreate(categoryData);
+  await Location.bulkCreate(locationData);
+  process.exit(0);
+};
+
+seedDatabase();
