24887e610b1790a5180d1da9c8a322102156d0d7
Oliver Saxon <oliverrsaxon@gmail.com>
2021-06-05 13:59:19 +0100

    Added home html route and main page styling with nav bar and footer

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -8,4 +8,9 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 });
 
+// render homepage
+router.get('/', (req, res) => {
+    res.render('homepage')
+})
+
 module.exports = router;
