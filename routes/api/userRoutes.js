c766468ad6d2cbe564afc30bbd115fedfefad19d
Ross White <rosswhite@outlook.com>
2021-06-06 11:12:56 +0100

    adding logout api route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const signupController = require('../../controllers/signupController');
 const loginController = require('../../controllers/loginController');
-
+const logoutController = require('../../controllers/logoutController');
 
 router.post('/', signupController);
 router.post('/login', loginController);
+router.post('logout', logoutController);
 
 
 module.exports = router;
