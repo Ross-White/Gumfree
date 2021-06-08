15429594892892a43c110675a8cc5e7759967980
Oliver Saxon <oliverrsaxon@gmail.com>
2021-06-08 14:13:08 +0100

    Dashboard routes file created

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const apiRoutes = require('./api');
 const itemRoutes = require('./itemRoutes')
+const dashboardRoutes = require('./dashboardRoutes')
 
 router.use('/', homeRoutes);
 router.use('/items', itemRoutes);
 router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 
 module.exports = router;
