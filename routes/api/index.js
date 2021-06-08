40df89041c5e505eb1efe150b6ad88d2f6181d43
Oliver Saxon <oliverrsaxon@gmail.com>
2021-06-08 14:54:18 +0100

    new Item route added to index.js API

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes.js');
+const itemRoutes = require('./itemRoutes.js')
 
 router.use('/users', userRoutes);
+router.use('/items', itemRoutes)
 
 module.exports = router;
