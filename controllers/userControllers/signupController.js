d1ffc2b3ca05c52326ef1a9f613357a77293308e
Ross White <rosswhite@outlook.com>
2021-06-08 21:32:29 +0100

    adding logged session to signup

--- controllers/userControllers/signupController.js
+++ controllers/userControllers/signupController.js
@@ -12,6 +12,11 @@ const signupController = async (req, res) => {
             password: req.body.password,
             location: point
         });
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
         res.status(200).json(userData);
     } catch (err) {
         res.status(400).json(err);
