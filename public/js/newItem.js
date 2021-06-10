0a8e94ce2b8a09acbac70e2bd09b08d29c6b3b2c
Luke Drogan <luke@super-wide.com>
2021-06-10 15:39:47 +0100

    updated form + item script

--- public/js/newItem.js
+++ public/js/newItem.js
@@ -23,7 +23,31 @@ const uploadS3 = multer({
       }
     })
   })
-
-
-
   module.exports = uploadS3;
+
+  const newItemFormHandler = async (event) => {
+    event.preventDefault();
+    const title = document.querySelector('#title').value.trim();
+    const desc = document.querySelector('#desription').value.trim();
+    const qty = document.querySelector('#quantity').value.trim();
+    const category = document.querySelector('#category').value.trim();
+    const offered = document.querySelector('#offered').value.trim();
+    if (title && desc && qty && category && offered) {
+        const response = await fetch('/api/users', {
+          method: 'POST',
+          body: JSON.stringify({ title, desc, qty, category, offered }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+        console.log(response);   
+        if (response.ok) {
+          document.location.replace('/');
+        } else {
+          alert(response.statusText);
+        }
+      }
+}; 
+
+document
+  .querySelector("#new-ad-form")
+  .addEventListener('submit', newItemFormHandler);
+
