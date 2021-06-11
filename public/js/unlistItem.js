c5462e5b4fd433b91026cc3d8db60ff0eb6f46ff
Oliver Saxon <oliverrsaxon@gmail.com>
2021-06-11 13:36:53 +0100

    Response from API call working

--- public/js/unlistItem.js
+++ public/js/unlistItem.js
@@ -1,8 +1,21 @@
-const unlistBtn = document.querySelector('#unlist')
+const unlistBtn = document.querySelector("#unlist");
 
-unlistBtn.addEventListener('click', (event)=>{
+
+const unlistItemHandler = async (event) => {
     event.preventDefault();
     const itemId = document.querySelector("#itemID").textContent;
-    console.log("unlist", itemId)
-    const response = await fetch(`/api/items/${itemId}`)
-})
+    console.log("unlist", itemId);
+    const response = await fetch(`/api/items/${itemId}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        available: false,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log(response)
+    if(response.ok){
+        document.location.replace('/dashboard')
+    }
+}
+
+unlistBtn.addEventListener("click", unlistItemHandler);
